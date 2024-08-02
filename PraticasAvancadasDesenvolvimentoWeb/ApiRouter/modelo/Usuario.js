d8ec470d8faffc2369c81f8310d281705815402a
davibschneid <davi.schneid@gmail.com>
2024-08-02 17:57:36 -0300

    Enviando as alteracoes no modelo e controller com os campos de CEP, endereco, bairro, estado

--- PraticasAvancadasDesenvolvimentoWeb/ApiRouter/modelo/Usuario.js
+++ PraticasAvancadasDesenvolvimentoWeb/ApiRouter/modelo/Usuario.js
@@ -1,48 +1,68 @@
- 
+
 const Sequelize = require('sequelize');
 const database = require('../data_base/db');
- 
+
 const Usuario = database.define('usuario', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  nome: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  idade: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  cidade: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  uf: {
+    type: Sequelize.STRING,
+    allowNull: true
+  },
+  cep: {
+    type: Sequelize.STRING,
+    allowNull: true
+  },
+  complemento: {
+    type: Sequelize.STRING,
+    allowNull: true
+  },
+  bairro: {
+    type: Sequelize.STRING,
+    allowNull: true
+  },
+  numero: {
+    type: Sequelize.INTEGER,
+    allowNull: true
+  }
+}, {
+
+
+
+  // Configurações do modelo
+  timestamps: true, // Habilita createdAt e updatedAt
+  hooks: {
+    beforeCreate: (usuario, options) => {
+      const now = new Date();
+      const threeHoursLater = new Date(now.getTime() - 3 * 60 * 60 * 1000);
+      usuario.createdAt = threeHoursLater;
+      usuario.updatedAt = threeHoursLater;
     },
-    nome: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    idade: {
-        type: Sequelize.INTEGER,
-        allowNull: false
-    },
-    cidade:  {
-        type: Sequelize.STRING,
-        allowNull: false
+    beforeUpdate: (usuario, options) => {
+      const now = new Date();
+      const threeHoursLater = new Date(now.getTime() - 3 * 60 * 60 * 1000);
+      usuario.updatedAt = threeHoursLater;
     }
-    }, {
+  }
 
-
-
-    // Configurações do modelo
-       timestamps: true, // Habilita createdAt e updatedAt
-        hooks: {
-          beforeCreate: (usuario, options) => {
-            const now = new Date();
-            const threeHoursLater = new Date(now.getTime() - 3 * 60 * 60 * 1000);
-            usuario.createdAt = threeHoursLater;
-            usuario.updatedAt = threeHoursLater;
-          },
-          beforeUpdate: (usuario, options) => {
-            const now = new Date();
-            const threeHoursLater = new Date(now.getTime() - 3 * 60 * 60 * 1000);
-            usuario.updatedAt = threeHoursLater;
-          }
-    }
-        
 })
 
 
- 
+
 module.exports = Usuario;
