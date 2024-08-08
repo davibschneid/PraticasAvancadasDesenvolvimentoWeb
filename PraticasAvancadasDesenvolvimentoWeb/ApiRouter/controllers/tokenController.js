6f4bbf5a2fe2380efe43d856c8d191521ee7593c
davibschneid <davi.schneid@gmail.com>
2024-08-08 11:16:24 -0300

    Atualizando a APIRouter com a autenticacao por token

--- PraticasAvancadasDesenvolvimentoWeb/ApiRouter/controllers/tokenController.js
+++ PraticasAvancadasDesenvolvimentoWeb/ApiRouter/controllers/tokenController.js
@@ -5,14 +5,22 @@ exports.validaToken = async (req, res) => {
     
     const { token } = req.body;
     console.log('Validar Token',token);
-    if (!token) {
-        res.status(400).json({ valid: false });
-    }
-
-    jwt.verify(token, process.env.JWT_KEY, (err, decoded) => {
-        if (err) {
-            res.status(401).json({ valid: false });
+    try{
+        if (!token) {
+            console.log('Retorna http 400');
+            res.status(400).json({ valid: false });
+        }else{
+            jwt.verify(token, process.env.JWT_KEY, (err, decoded) => {
+            if (err) {
+                console.log('Retorna http 401');
+                res.status(401).json({ valid: false });
+            }else{
+                console.log('Retorna http 200');
+                res.status(200).json({ valid: true });
+            }
+            });
         }
-        res.status(200).json({ valid: true });
-    });
+    }catch(err){
+        console.log('Erro ao validar token',err);
+    }
 };
